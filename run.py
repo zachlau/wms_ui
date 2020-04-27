fafa401fbe154d87c81e17b80e7ba54d219c1f89
Administrator <admin@example.com>
2020-04-27 18:35:10 +0800

    修改webhook

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     suite = unittest.TestLoader().discover(start_dir=path + '/cases', pattern='*.py')
     # unittest.TextTestRunner().run(suite)
     fp = open(path+'./reports/report.html', 'wb')
-    HTMLTestReportCN.HTMLTestRunner(stream=fp, title='仓库管理系统UI自动化测试报告').run(suite)
+    HTMLTestReportCN.HTMLTestRunner(stream=fp, title='仓库管理系统UI自动化测试报告1').run(suite)
     fp.close()
